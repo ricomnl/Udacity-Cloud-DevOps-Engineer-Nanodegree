49d588f1660e639b1d373059c3a471c31ab8b912
rmeinl <rmeinl97@googlemail.com>
2020-04-19 18:25:47 +0100

    added project

--- udacity-ml-microservice-kubernetes/README.md
+++ udacity-ml-microservice-kubernetes/README.md
@@ -1,4 +1,4 @@
-<include a CircleCI status badge, here>
+[![CircleCI](https://circleci.com/gh/rmeinl/udacity-ml-microservice-kubernetes.svg?style=svg)](https://circleci.com/gh/rmeinl/udacity-ml-microservice-kubernetes)
 
 ## Project Overview
 
--- udacity-ml-microservice-kubernetes/run_kubernetes.sh
+++ udacity-ml-microservice-kubernetes/run_kubernetes.sh
@@ -8,7 +8,7 @@ dockerpath=rmeinl/ml-microservice-kubernetes:latest
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-kubectl run --image=$dockerpath ml-microservice-kubernetes --port=80
+kubectl run ml-microservice-kubernetes --image=$dockerpath --port=80
 
 # Step 3:
 # List kubernetes pods
@@ -16,7 +16,7 @@ kubectl get pods
 
 # Step 4:
 # Forward the container port to a host
-kubectl expose deployment ml-microservice-kubernetes --port=8000 --target-port=80
+kubectl port-forward deployment/ml-microservice-kubernetes 8080:80
 
 # delete
 # kubectl delete pods --all
